0352c47d17a35438de03ce60b7398335e74b1600
Cristian Padureac <Moldoteck@users.noreply.github.com>
2021-06-20 01:32:00 +0300

    .

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -2,7 +2,7 @@ import { findUser } from '../models'
 import { Context } from 'telegraf'
 
 export async function attachUser(ctx: Context, next) {
-  const dbuser = undefined
+  let dbuser = undefined
   try{ dbuser= await findUser(ctx.from.id)} catch(err){console.log(err)}
   ctx.dbuser = dbuser
   return next()
